9a41cb5d68bf4c6b9708409a5bb260c0ed57c1d3
phi <phi1618jp@gmail.com>
2015-07-30 15:29:23 +0900

    fix paint

--- index.js
+++ index.js
@@ -8,35 +8,27 @@ var port = process.env.PORT || 3000
 app.use(express.static(__dirname + "/"))
 
 var server = app.listen(port, function() {
-    console.log('start');
+  console.log('start');
 });
 
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
 
-var connections = [];
-var broadcast = function(message) {
-    connections.forEach(function (con, i) {
-        con.send(message);
-    });
+wss.broadcast = function(data) {
+  this.clients.forEach(function(client) {
+    client.send(data);
+  });
 };
 
-
 wss.on("connection", function(ws) {
-  connections.push(ws);
-  var id = setInterval(function() {
-    // ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
 
   console.log("websocket connection open")
 
   ws.on("close", function() {
     console.log("websocket connection close")
-    clearInterval(id)
   })
 
   ws.on('message', function(message) {
     console.log('message', message);
-    broadcast(JSON.stringify(message));
+    wss.broadcast(message);
   });
 });
